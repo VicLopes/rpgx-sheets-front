0319b2ab752ef5aa4c0fef6a9fe7993b3e589878
Victor Lopes <viclopes2621@gmail.com>
2021-10-17 04:44:10 -0300

    separates playerdata from primarydata + sets up player context

--- src/shared/containers/PrimaryData.js
+++ src/shared/containers/PrimaryData.js
@@ -5,31 +5,23 @@ import '../styles/primarydata.scss'
 export const PrimaryData = ({ primaryDataObj }) => {
   return (
     <section id="primary-data">
-    <p>
-      Name:
-      <span className="character-name">{primaryDataObj.character_name}</span>
-    </p>
-    <p>
-      Player Name:
-      <span className="player-name">{primaryDataObj.player.player_name}</span>
-    </p>
-    <p>
-      Level: <span className="level">{primaryDataObj.level}</span>
-    </p>
-    <p>
-      Class: <span className="class">{primaryDataObj.class}</span>
-    </p>
-    <p>
-      Armor Class:
-      <span className="armor-class">{primaryDataObj.armor_class}</span>
-    </p>
-    <p>
-      Hit Points: <span className="hit-points">{primaryDataObj.hit_points}</span>
-    </p>
-    <p>
-      Ancestry: <span className="ancestry">{primaryDataObj.ancestry}</span>
-    </p>
-  </section>
+      <p>
+        Level: <span className="level">{primaryDataObj.level}</span>
+      </p>
+      <p>
+        Class: <span className="class">{primaryDataObj.class}</span>
+      </p>
+      <p>
+        Armor Class:
+        <span className="armor-class">{primaryDataObj.armor_class}</span>
+      </p>
+      <p>
+        Hit Points: <span className="hit-points">{primaryDataObj.hit_points}</span>
+      </p>
+      <p>
+        Ancestry: <span className="ancestry">{primaryDataObj.ancestry}</span>
+      </p>
+    </section>
   )
 }
 
--- src/shared/services/fetchData.js
+++ src/shared/services/fetchData.js
@@ -1,7 +1,21 @@
 import CONFIG from '../../config/envs.config'
 
-export const getSheet = async (data = {}) => {
-  const response = await fetch(CONFIG.API_URL, {
+export const getSheet = async () => {
+  const response = await fetch(CONFIG.API_URL+'/pathfinder', {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer'
+  })
+
+  return response.json()
+}
+
+export const getPlayerData = async (playerId = 1) => {
+  const response = await fetch(`${CONFIG.API_URL}player/${playerId}`, {
     method: 'GET',
     mode: 'cors',
     headers: {
